f9b13fdbff17aa5fd2661865aab6061260fcef92
Mahad Ansar <mahadansar@gmail.com>
2020-07-16 15:03:51 +0500

    Singleton Pattern

---
+++ Creational/Singleton/index.js
@@ -0,0 +1,26 @@
+var Logger = require("./Logger");
+var Shopper = require("./Shopper");
+var Store = require("./Store");
+
+var logger = new Logger().getInstance();
+
+logger.log("App Starting...");
+
+var alex = new Shopper("alex", 800);
+var ski_shop = new Store("Big O", [
+  {
+    item: "A",
+    qty: 5,
+    price: 567,
+  },
+  {
+    item: "BB",
+    qty: 3,
+    price: 313,
+  },
+]);
+
+logger.log("App Config Finished...");
+
+console.log(`${logger.count} logs total.`);
+logger.logs.map((log) => console.log(`${log.message}`));
